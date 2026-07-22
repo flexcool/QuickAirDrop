df5f496fa846899f2c3afe90b59b2816b8e5f8b3
Jun <wongjun@qq.com>
2026-07-22 15:46:16 +0800

    Minimal status bar test - text only

--- QuickAirDrop/Sources/App/AppDelegate.swift
+++ QuickAirDrop/Sources/App/AppDelegate.swift
@@ -1,82 +1,9 @@
 import Cocoa
-import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    private var statusBarController: StatusBarController!
-    private var launchAtLoginManager: LaunchAtLoginManager!
-    private var globalHotkeyManager: GlobalHotkeyManager!
-    private var clipboardMonitor: ClipboardMonitor!
-    private var settingsWindow: NSWindow?
-    private var historyWindow: NSWindow?
-
     func applicationDidFinishLaunching(_ notification: Notification) {
-        statusBarController = StatusBarController()
-        statusBarController.setup()
-
-        launchAtLoginManager = LaunchAtLoginManager()
-        launchAtLoginManager.syncState()
-
-        globalHotkeyManager = GlobalHotkeyManager()
-        globalHotkeyManager.registerDefault()
-
-        clipboardMonitor = ClipboardMonitor()
-        if UserDefaults.standard.bool(forKey: "clipboardMonitoringEnabled") {
-            clipboardMonitor.start()
-        }
-
-        NotificationManager.shared.requestAuthorization()
-    }
-
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return false
-    }
-
-    func showSettings() {
-        if settingsWindow == nil {
-            let settingsView = SettingsView()
-            let hostingController = NSHostingController(rootView: settingsView)
-            let window = NSWindow(contentViewController: hostingController)
-            window.title = "QuickAirDrop 设置"
-            window.styleMask = [.titled, .closable]
-            window.setContentSize(NSSize(width: 450, height: 400))
-            window.center()
-            settingsWindow = window
-        }
-        settingsWindow?.makeKeyAndOrderFront(nil)
-        NSApp.activate(ignoringOtherApps: true)
-    }
-
-    func showHistory() {
-        if historyWindow == nil {
-            let historyView = HistoryView()
-            let hostingController = NSHostingController(rootView: historyView)
-            let window = NSWindow(contentViewController: hostingController)
-            window.title = "发送历史"
-            window.styleMask = [.titled, .closable, .resizable]
-            window.setContentSize(NSSize(width: 500, height: 400))
-            window.center()
-            historyWindow = window
-        }
-        historyWindow?.makeKeyAndOrderFront(nil)
-        NSApp.activate(ignoringOtherApps: true)
-    }
-
-    func quit() {
-        clipboardMonitor.stop()
-        globalHotkeyManager.unregister()
-        NSApp.terminate(nil)
-    }
-}
-
-extension AppDelegate: NSWindowDelegate {
-    func windowWillClose(_ notification: Notification) {
-        if let window = notification.object as? NSWindow {
-            if window == settingsWindow {
-                settingsWindow = nil
-            } else if window == historyWindow {
-                historyWindow = nil
-            }
-        }
+        let controller = StatusBarController()
+        controller.setup()
     }
 }
--- QuickAirDrop/Sources/StatusBar/StatusBarController.swift
+++ QuickAirDrop/Sources/StatusBar/StatusBarController.swift
@@ -4,116 +4,15 @@ class StatusBarController: NSObject {
     private var statusItem: NSStatusItem!
 
     func setup() {
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
-        if let button = statusItem.button {
-            if let img = NSImage(systemSymbolName: "arrow.up.circle", accessibilityDescription: "QuickAirDrop") {
-                button.image = img
-            } else {
-                button.title = "⬆"
-            }
-            button.toolTip = "QuickAirDrop - 拖拽文件到此处"
-        }
+        statusItem.button?.title = "⬆"
+        statusItem.button?.toolTip = "QuickAirDrop"
 
-        statusItem.menu = buildMenu()
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.registerDragOnWindow()
-        }
-    }
-
-    private func registerDragOnWindow() {
-        guard let window = statusItem.button?.window else { return }
-        window.registerForDraggedTypes([.fileURL, .URL, .string])
-    }
-
-    private func handleDroppedFiles(_ files: [URL]) {
-        let validatedFiles = FileValidator.shared.filterValidFiles(files)
-        guard !validatedFiles.isEmpty else {
-            NotificationManager.shared.show(
-                title: "无有效文件",
-                message: "所选文件类型不受支持"
-            )
-            return
-        }
-        AirDropManager.shared.sendViaAirDrop(files: validatedFiles)
-    }
-
-    private func buildMenu() -> NSMenu {
         let menu = NSMenu()
-
-        let titleItem = NSMenuItem(title: "QuickAirDrop", action: nil, keyEquivalent: "")
-        titleItem.isEnabled = false
-        menu.addItem(titleItem)
-
+        menu.addItem(NSMenuItem(title: "QuickAirDrop", action: nil, keyEquivalent: ""))
         menu.addItem(.separator())
-
-        let settingsItem = NSMenuItem(title: "设置...", action: #selector(showSettings), keyEquivalent: ",")
-        settingsItem.target = self
-        menu.addItem(settingsItem)
-
-        let historyItem = NSMenuItem(title: "发送历史", action: #selector(showHistory), keyEquivalent: "h")
-        historyItem.target = self
-        menu.addItem(historyItem)
-
-        menu.addItem(.separator())
-
-        let launchItem = NSMenuItem(
-            title: "开机启动",
-            action: #selector(toggleLaunchAtLogin),
-            keyEquivalent: ""
-        )
-        launchItem.target = self
-        launchItem.state = LaunchAtLoginManager.isEnabled ? .on : .off
-        menu.addItem(launchItem)
-
-        menu.addItem(.separator())
-
-        let quitItem = NSMenuItem(title: "退出", action: #selector(quit), keyEquivalent: "q")
-        quitItem.target = self
-        menu.addItem(quitItem)
-
-        return menu
-    }
-
-    @objc func handleDragOperation(_ sender: NSDraggingInfo) -> Bool {
-        guard let urls = sender.draggingPasteboard.readObjects(
-            forClasses: [NSURL.self],
-            options: nil
-        ) as? [URL], !urls.isEmpty else {
-            return false
-        }
-        let fileURLs = urls.filter { $0.isFileURL }
-        guard !fileURLs.isEmpty else { return false }
-
-        DispatchQueue.main.async {
-            self.handleDroppedFiles(fileURLs)
-        }
-        return true
-    }
-
-    @objc private func showSettings() {
-        (NSApp.delegate as? AppDelegate)?.showSettings()
-    }
-
-    @objc private func showHistory() {
-        (NSApp.delegate as? AppDelegate)?.showHistory()
-    }
-
-    @objc private func toggleLaunchAtLogin() {
-        do {
-            try LaunchAtLoginManager.toggle()
-            statusItem.menu?.item(withTitle: "开机启动")?.state =
-                LaunchAtLoginManager.isEnabled ? .on : .off
-        } catch {
-            NotificationManager.shared.show(
-                title: "设置失败",
-                message: error.localizedDescription
-            )
-        }
-    }
-
-    @objc private func quit() {
-        (NSApp.delegate as? AppDelegate)?.quit()
+        menu.addItem(NSMenuItem(title: "退出", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        statusItem.menu = menu
     }
 }
