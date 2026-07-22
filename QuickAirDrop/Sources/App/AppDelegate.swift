d70860a0213a83c1d1c0186c6ec7dd72c0414383
Jun <wongjun@qq.com>
2026-07-22 16:25:01 +0800

    Add showSettings/showHistory back to AppDelegate

--- QuickAirDrop/Sources/App/AppDelegate.swift
+++ QuickAirDrop/Sources/App/AppDelegate.swift
@@ -12,4 +12,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         false
     }
+
+    func showSettings() {
+        let view = SettingsView()
+        let hostingController = NSHostingController(rootView: view)
+        let window = NSWindow(contentViewController: hostingController)
+        window.title = "设置"
+        window.styleMask = [.titled, .closable]
+        window.setContentSize(NSSize(width: 400, height: 300))
+        window.center()
+        window.makeKeyAndOrderFront(nil)
+        NSApp.activate(ignoringOtherApps: true)
+    }
+
+    func showHistory() {
+        let view = HistoryView()
+        let hostingController = NSHostingController(rootView: view)
+        let window = NSWindow(contentViewController: hostingController)
+        window.title = "发送历史"
+        window.styleMask = [.titled, .closable]
+        window.setContentSize(NSSize(width: 400, height: 300))
+        window.center()
+        window.makeKeyAndOrderFront(nil)
+        NSApp.activate(ignoringOtherApps: true)
+    }
 }
