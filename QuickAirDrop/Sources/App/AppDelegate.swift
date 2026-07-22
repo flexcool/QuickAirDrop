c7bafdd386673b9e13e2a9980116edb144357814
Jun <wongjun@qq.com>
2026-07-22 15:53:45 +0800

    Move entry point to AppDelegate, add logging for debugging

--- QuickAirDrop/Sources/App/AppDelegate.swift
+++ QuickAirDrop/Sources/App/AppDelegate.swift
@@ -1,9 +1,18 @@
 import Cocoa
 
-@NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
+        NSLog("QuickAirDrop: applicationDidFinishLaunching called")
         let controller = StatusBarController()
         controller.setup()
+        NSLog("QuickAirDrop: StatusBarController setup complete")
     }
 }
+
+NSLog("QuickAirDrop: Starting...")
+let app = NSApplication.shared
+let delegate = AppDelegate()
+app.delegate = delegate
+app.setActivationPolicy(.accessory)
+NSLog("QuickAirDrop: Calling app.run()")
+app.run()
