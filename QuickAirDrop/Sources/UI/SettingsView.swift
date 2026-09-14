71610ae96697b73a812c2702c433a2be417a3752
Jun <wongjun@qq.com>
2026-09-14 18:21:20 +0800

    Revert "移除formContentFrame的padding,避免内容溢出时顶部多出空白"
    This reverts commit 63f4653db357aaa05f3d90c4bd19fa3f9b52ec27.

--- QuickAirDrop/Sources/UI/SettingsView.swift
+++ QuickAirDrop/Sources/UI/SettingsView.swift
@@ -11,6 +11,7 @@ private struct SectionHeaderStyle: ViewModifier {
 private struct FormContentFrame: ViewModifier {
     func body(content: Content) -> some View {
         content
+            .padding()
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
     }
 }
